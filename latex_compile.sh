8927138561ae366a67669eda9846420ca47075b4
Joel Gillette <joelliuscaesar@gmail.com>
2019-01-30 17:54:25 -0500

    Renamed script and made it more extensible. Fixed an error with phone number. Added a gitignore

--- latex_compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pdflatex "\def\fname{Dude} \def\lname{Lebowsky} \def\myemail{test@test.test} \def\mophone{555-555-5555} \input{cv_8.tex}"
---
+++ resume_compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  --fname=*)
+    fnarg="${1#*=}"
+    ;;
+  --lname=*)
+    lnarg="${1#*=}"
+    ;;
+  --myemail=*)
+    emarg="${1#*=}"
+    ;;
+  --mophone=*)
+    pharg="${1#*=}"
+    ;;
+  *) : ;;
+  esac
+  shift
+done
+
+fname="${fnargs:-Dude}"
+lname="${lnargs:-Lebowsky}"
+myemail="${emarg:-test@test.com}"
+mophone="${pharg:-(555)555-5555}"
+
+pdflatex "\def\fname{$fname} \def\lname{$lname} \def\myemail{$myemail} \def\mophone{$mophone} \input{cv_8.tex}"
